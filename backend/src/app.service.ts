0c351a24e110cdd115c99358675712d772b5daa9
Darwish1231 <darwish1231@github.com>
2026-04-08 03:49:49 +0200

    debug: version stamp for backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -7,6 +7,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Rased API Version 5.2 - Live';
   }
 }
